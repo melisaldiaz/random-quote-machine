577af083a87ebb35fab356524127637130791298
melisaldiaz <melisaldiaz@gmail.com>
2020-07-30 18:52:20 +0300

    random quote machine

--- src/App.js
+++ src/App.js
@@ -1,26 +1,134 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import myQuotes from './Quotes';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+//Action type
+const SET_QUOTE = "SET_QUOTE";
+
+
+// Initial State
+const initialState = {
+  quote: myQuotes[Math.floor(Math.random() * myQuotes.length)]
+};
+
+//Action creator
+function randomQuoteAction() {
+  const i = Math.floor(Math.random() * myQuotes.length);
+  return {
+    type: SET_QUOTE,
+    index: i
+  };
 }
 
-export default App;
+//Reducer
+
+function reducer(state, action) {
+  switch (action.type) {
+    case SET_QUOTE:
+      return {
+        quote: myQuotes[action.index]
+      };
+    default:
+      return state;
+  }
+}
+
+
+function Quote(props) {
+
+  const [state, dispatch] = React.useReducer(reducer, initialState);
+
+  const twitterUrl = encodeURI("https://twitter.com/intent/tweet?text=" + state.quote.text + " — " + state.quote.author);
+
+  return (
+    <div class="container-fluid">
+      <div class="row justify-content-center">
+        <div class="col-md-7 col-sm-7">
+          <blockquote class="text-center clearfix" id="quote-box">
+            <p id="text">{state.quote.text}</p>
+            <footer class="blockquote-footer text-right" id="author">
+              {state.quote.author}
+            </footer>
+
+            <a
+              class="button"
+              id="tweet-quote"
+              title="Share this quote!"
+              target="_blank"
+              href={twitterUrl}
+            >
+              <i class="fab fa-twitter"></i>
+            </a>
+            <button
+              onClick={() => dispatch(randomQuoteAction())}
+              class="btn btn-dark"
+              id="new-quote"
+            >
+              New quote
+            </button>
+          </blockquote>
+        </div>
+      </div>
+    </div>
+  );
+
+
+}
+
+
+/* class Quote extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick(e) {
+    e.preventDefault();
+    this.props.getNextQuote();
+  }
+
+  render() {
+    return (
+      <div class="row justify-content-center">
+        <div class="col-md-7 col-sm-7">
+          <blockquote class="text-center clearfix" id="quote-box">
+            <p id="text">{this.props.text}</p>
+            <footer class="blockquote-footer text-right" id="author">
+              {this.props.author}
+            </footer>
+
+            <a
+              class="button"
+              id="tweet-quote"
+              title="Share this quote!"
+              target="_blank"
+              href={encodeURI(
+                "https://twitter.com/intent/tweet?text=" +
+                this.props.text +
+                " — " +
+                this.props.author
+              )}
+            >
+              <i class="fab fa-twitter"></i>
+            </a>
+            <button
+              onClick={this.handleClick}
+              class="btn btn-dark"
+              id="new-quote"
+            >
+              New quote
+            </button>
+          </blockquote>
+        </div>
+      </div>
+    );
+  }
+} */
+
+
+
+
+export default Quote;
---
+++ src/Quotes.js
@@ -0,0 +1,81 @@
+//Given an author and a quote, return a quote object.
+function makeQuote(a, t) {
+    return {
+        text: t,
+        author: a
+    };
+}
+
+// Array of quote objects.
+export default [
+    makeQuote("Oscar Wilde", "Be yourself; everyone else is already taken."),
+    makeQuote("Frank Zappa", "So many books, so little time."),
+    makeQuote(
+        "Albert Einstein",
+        "Two things are infinite: the universe and human stupidity; and I'm not sure about the universe."
+    ),
+    makeQuote(
+        "Mae West",
+        "You only live once, but if you do it right, once is enough."
+    ),
+    makeQuote(
+        "Mahatma Gandhi",
+        "Be the change that you wish to see in the world."
+    ),
+    makeQuote(
+        "Eleanor Roosevelt",
+        "No one can make you feel inferior without your consent."
+    ),
+    makeQuote(
+        "Mark Twain",
+        "Good friends, good books, and a sleepy conscience: this is the ideal life."
+    ),
+    makeQuote(
+        "Neil Gaiman",
+        "Fairy tales are more than true: not because they tell us that dragons exist, but because they tell us that dragons can be beaten."
+    ),
+    makeQuote(
+        "J.K. Rowling",
+        "To the well-organized mind, death is but the next great adventure."
+    ),
+    makeQuote(
+        "Roald Dahl",
+        "Those who don't believe in magic will never find it."
+    ),
+    makeQuote(
+        "Guy de Maupassant",
+        "Our memory is a more perfect world than the universe: it gives back life to those who no longer exist."
+    ),
+    makeQuote(
+        "Sylvia Plath",
+        "I shut my eyes and all the world drops dead; I lift my eyes and all is born again."
+    ),
+    makeQuote(
+        "Terry Pratchett",
+        "Wisdom comes from experience. Experience is often a result of lack of wisdom."
+    ),
+    makeQuote(
+        "Haruki Murakami",
+        "It's like Tolstoy said. Happiness is an allegory, unhappiness a story."
+    ),
+    makeQuote(
+        "Kurt Vonnegut",
+        "And I asked myself about the present: how wide it was, how deep it was, how much was mine to keep."
+    ),
+    makeQuote(
+        "George Orwell",
+        "If liberty means anything at all, it means the right to tell people what they do not want to hear."
+    ),
+    makeQuote(
+        "George Carlin",
+        "If you try to fail, and succeed, which have you done?"
+    ),
+    makeQuote(
+        "John Updike",
+        "Dreams come true. Without that possibility, nature would not incite us to have them."
+    ),
+    makeQuote(
+        "Vladimir Nabokov",
+        "Our imagination flies — we are its shadow on the earth."
+    )
+];
